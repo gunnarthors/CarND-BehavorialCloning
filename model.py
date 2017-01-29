c8c03c57f398e78319e0633d48779d26c0fe84d8
gunnarthors <gunnarthors@gmail.com>
2017-01-29 23:29:09 +0000

    trained again

--- model.py
+++ model.py
@@ -150,8 +150,8 @@ def main():
     path = '/data/driving_log.csv'
     training_data = prepareDataFromCSV(os.getcwd() + path)
     batch_size = 128
-    samples_per_epoch = batch_size * 160
-    nb_epoch = 10
+    samples_per_epoch = batch_size * 320
+    nb_epoch = 8
     print(" Training data from csv: {}".format(path))
     print(" Batch size: {} \n Number of epochs: {} \n Samples per epoch {}"
         .format(batch_size, nb_epoch, samples_per_epoch))
